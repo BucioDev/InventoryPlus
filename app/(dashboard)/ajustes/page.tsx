405055831956873f446306c854061fe2b6030be8
BucioDev <buciodev@gmail.com>
2025-12-10 18:58:20 -0800

    hidden button for reports since is not finish

--- app/(dashboard)/ajustes/page.tsx
+++ app/(dashboard)/ajustes/page.tsx
@@ -18,9 +18,6 @@ export default function AjustesPage() {
                     <Button asChild>
                         <Link href="/ajustes/logs">Resgistros de actividad</Link>
                     </Button>
-                     <Button asChild>
-                        <Link href="/ajustes/reportes">Reportes</Link>
-                    </Button>
                 </CardContent>
             </Card>
         </div>
