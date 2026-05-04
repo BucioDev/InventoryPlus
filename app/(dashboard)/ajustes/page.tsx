b3666f029024d3515146b57aa98b1de948768f38
BucioDev <buciodev@gmail.com>
2026-05-04 14:04:27 -0700

    fix error on ajustes, where use client and async collition

--- app/(dashboard)/ajustes/page.tsx
+++ app/(dashboard)/ajustes/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 import { isLoggedIn } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
